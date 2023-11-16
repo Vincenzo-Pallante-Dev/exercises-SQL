937928fc1d5ead8c724971bfde95cc8348e6c36b
Vincenzo <vincenzopallante@icoud.com>
2023-11-16 19:12:16 +0100

    feat: exercise

---
+++ src/db.ts
@@ -0,0 +1,24 @@
+import pgPromise from "pg-promise";
+
+const db = pgPromise()(
+  "postgres://postgres:postgres@localhost:5432/exercise-SQL"
+);
+
+const setupDb = async () => {
+  await db.none(`
+  DROP TABLE IF EXISTS books;
+
+    CREATE TABLE books (
+        book_id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        author TEXT NOT NULL,
+        genre TEXT NOT NULL,
+        published_year INTEGER NOT NULL,
+        isbn INTEGER NOT NULL,
+        price INTEGER NOT NULL,
+        rating INTEGER NOT NULL,
+        stock_count INTEGER NOT NULL
+    )
+    `);
+};
+setupDb();
